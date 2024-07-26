41f3cc927c9b433fae71106b1ee13ae508cb78aa
Fabricio Luiz Rodrigues <fabricioflr@gmail.com>
2024-07-26 18:07:55 -0300

    upload files

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function(){
+/*document.addEventListener('DOMContentLoaded', function(){
+
     const fotoPerfil = document.querySelector('#fotoPerfil');
     const nome = document.querySelector('#nome');
     const nomeUsuario = document.querySelector('#nome-usuario');
@@ -7,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const seguindo = document.querySelector('#seguindo');
     const link = document.querySelector('#link');
 
-    fetch('https://api.github.com/users/Fabricio-luiz')
+    fetch('https://api.github.com/users/${}')
         .then(function(resposta){
             return resposta.json();
         })
@@ -19,6 +20,49 @@ document.addEventListener('DOMContentLoaded', function(){
             seguidores.innerText = json.followers;
             seguindo.innerText = json.following;
             link.href = json.html_url;
+        })  
+})*/
+
+$(document).ready(function(){
+
+    $('#btn-buscar-user').click(function(){
+        const user = $('#user').val();
+        const endpoint = `https://api.github.com/users/${user}`;
+        const botao = $(this);
+        const fotoPerfil = document.querySelector('#fotoPerfil');
+        const nome = document.querySelector('#nome');
+        const nomeUsuario = document.querySelector('#nome-usuario');
+        const repositorio = document.querySelector('#repositorio');
+        const seguidores = document.querySelector('#seguidores');
+        const seguindo = document.querySelector('#seguindo');
+        const link = document.querySelector('#link');
+        $(botao).find('i').addClass('d-none');
+        $(botao).find('span').removeClass('d-none');
+
+        fetch(endpoint)
+        .then(function(resposta){
+            return resposta.json();
         })
-        
+
+        .then(function(json){
+            fotoPerfil.src = json.avatar_url;
+            nome.innerText = json.name;
+            nomeUsuario.innerText = json.login;
+            repositorio.innerText = json.public_repos;
+            seguidores.innerText = json.followers;
+            seguindo.innerText = json.following;
+            link.href = json.html_url;
+        })
+
+        .catch(function(erro){
+            alert("Usuário não encontrado, tente novamente.")
+        })
+
+        .finally(function(){
+            setTimeout(function(){
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('span').addClass('d-none');
+            }, 1000);
+        })
+    })
 })
