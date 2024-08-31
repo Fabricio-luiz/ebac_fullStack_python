9e9e9620ecd4d3635a5330394dd82e83326bcff3
Fabricio Luiz Rodrigues <fabricioflr@gmail.com>
2024-08-31 17:26:16 -0300

    upload file main.js - Alunos aprovados

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,25 @@
 "use strict";
 
-console.log('Olá');
+var alunos = [{
+  nome: "Maria",
+  nota: 9
+}, {
+  nome: "Andre",
+  nota: 4
+}, {
+  nome: "Paulo",
+  nota: 6
+}, {
+  nome: "Vanessa",
+  nota: 7
+}, {
+  nome: "Cleber",
+  nota: 8
+}];
+function filtrarAprovados(alunos) {
+  return alunos.filter(function (aluno) {
+    return aluno.nota >= 6;
+  });
+}
+var alunosAprovados = filtrarAprovados(alunos);
+console.log(alunosAprovados);
--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log('Olá');
+const alunos = [
+    { nome: "Maria", nota: 9 },
+    { nome: "Andre", nota: 4 },
+    { nome: "Paulo", nota: 6 },
+    { nome: "Vanessa", nota: 7 },
+    { nome: "Cleber", nota: 8 }
+];
+
+function filtrarAprovados(alunos) {
+    return alunos.filter(aluno => aluno.nota >= 6);
+}
+
+const alunosAprovados = filtrarAprovados(alunos);
+console.log(alunosAprovados);
